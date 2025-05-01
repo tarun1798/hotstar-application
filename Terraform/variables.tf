de0722193089a95d108e7d5b278ce811c64c5b28
tarun1798 <tarunb617@gmail.com>
2025-05-01 19:03:21 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "terra"                  # Names of key in aws
+  default = "eks-cli"                  # Names of key in aws
 }
 
 
